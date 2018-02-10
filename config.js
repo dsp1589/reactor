82e489f393c57e8d5b470db82545cf5221332b56
dhana <Dsp@bitbucket1>
2018-02-10 18:37:24 -0500

    ex rate changes

---
+++ config.js
@@ -0,0 +1,4 @@
+
+let PORT = '9090';
+
+export default PORT;
---
+++ server.js
@@ -0,0 +1,38 @@
+import config from './config';
+import express from 'express';
+import https from 'https';
+import JSONStream from 'JSONStream';
+const server = express();
+
+server.get('/refreshRate',(req,res)=>{
+  var url = "https://openexchangerates.org/api/latest.json?app_id="+"&base="+"USD"+"&symbols="+"AED,AFN,ALL,AMD,ANG,AOA,ARS,AUD,AWG,AZN,BAM,BBD,BDT,BGN,BHD,BIF,BMD,BND,BOB,BRL,BSD,BTC,BTN,BWP,BYN,BZD,CAD,CDF,CHF,CLF,CLP,CNH,CNY,COP,CRC,CUC,CUP,CVE,CZK,DJF,DKK,DOP,DZD,EGP,ERN,ETB,EUR,FJD,FKP,GBP,GEL,GGP,GHS,GIP,GMD,GNF,GTQ,GYD,HKD,HNL,HRK,HTG,HUF,IDR,ILS,IMP,INR,IQD,IRR,ISK,JEP,JMD,JOD,JPY,KES,KGS,KHR,KMF,KPW,KRW,KWD,KYD,KZT,LAK,LBP,LKR,LRD,LSL,LYD,MAD,MDL,MGA,MKD,MMK,MNT,MOP,MRO,MRU,MUR,MVR,MWK,MXN,MYR,MZN,NAD,NGN,NIO,NOK,NPR,NZD,OMR,PAB,PEN,PGK,PHP,PKR,PLN,PYG,QAR,RON,RSD,RUB,RWF,SAR,SBD,SCR,SDG,SEK,SGD,SHP,SLL,SOS,SRD,SSP,STD,STN,SVC,SYP,SZL,THB,TJS,TMT,TND,TOP,TRY,TTD,TWD,TZS,UAH,UGX,USD,UYU,UZS,VEF,VND,VUV,WST,XAF,XAG,XAU,XCD,XDR,XOF,XPD,XPF,XPT,YER,ZAR,ZMW,ZWL";
+  https.get(url,(res)=>{
+    var buffs = []
+    res.on('data',chunk => {
+      buffs.push(chunk);
+    }).on('end',()=>{
+      let json = JSON.parse(Buffer.concat(buffs).toString());
+      let queriedFor = "AED,AFN,ALL,AMD,ANG,AOA,ARS,AUD,AWG,AZN,BAM,BBD,BDT,BGN,BHD,BIF,BMD,BND,BOB,BRL,BSD,BTC,BTN,BWP,BYN,BZD,CAD,CDF,CHF,CLF,CLP,CNH,CNY,COP,CRC,CUC,CUP,CVE,CZK,DJF,DKK,DOP,DZD,EGP,ERN,ETB,EUR,FJD,FKP,GBP,GEL,GGP,GHS,GIP,GMD,GNF,GTQ,GYD,HKD,HNL,HRK,HTG,HUF,IDR,ILS,IMP,INR,IQD,IRR,ISK,JEP,JMD,JOD,JPY,KES,KGS,KHR,KMF,KPW,KRW,KWD,KYD,KZT,LAK,LBP,LKR,LRD,LSL,LYD,MAD,MDL,MGA,MKD,MMK,MNT,MOP,MRO,MRU,MUR,MVR,MWK,MXN,MYR,MZN,NAD,NGN,NIO,NOK,NPR,NZD,OMR,PAB,PEN,PGK,PHP,PKR,PLN,PYG,QAR,RON,RSD,RUB,RWF,SAR,SBD,SCR,SDG,SEK,SGD,SHP,SLL,SOS,SRD,SSP,STD,STN,SVC,SYP,SZL,THB,TJS,TMT,TND,TOP,TRY,TTD,TWD,TZS,UAH,UGX,USD,UYU,UZS,VEF,VND,VUV,WST,XAF,XAG,XAU,XCD,XDR,XOF,XPD,XPF,XPT,YER,ZAR,ZMW,ZWL";
+      let query = "insert into ex_rate ("+queriedFor+") values(";
+      var vc = "";
+      let splitted = queriedFor.split(',')
+      for (var i in splitted){
+        let fetched = json["rates"][splitted[i]];
+        if (fetched == 'undefined'){
+          vc += null;
+        }else{
+          vc +=  fetched;
+        }
+        vc += ",";
+      }
+      vc = vc.substring(0, vc.length - 1);
+
+      console.log(query.replace("ALL","\`ALL\`")+vc+")");
+    });
+  });
+  res.send("hello world");
+});
+
+server.listen(9090,()=>{
+  console.log("Listening at 9090");
+});
